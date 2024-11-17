b1bfd9d54de9de81f698bab30b3232246de3ae37
KaushikOP <kaushiktalathi@gmail.com>
2024-11-17 20:56:01 +0530

    Client updates

--- com/iot-digital-watermark/client/client.py
+++ com/iot-digital-watermark/client/client.py
@@ -30,18 +30,22 @@ class WatermarkClient:
 
                 # Send data to the server
                 self.send_data_to_server()
+
+            
         except KeyboardInterrupt:
             print("\nClient stopped by user.")
         finally:
             self.communication.close_client()
 
     def send_data_to_server(self):
-        file_path = self.file_handler.get_host_media()
-        watermarked_file_path = self.embedding.embed_watermark(file_path)
-        self.communication.send_file(watermarked_file_path)
+        self.communication.client_socket.send("Hello")
+        # file_path = self.file_handler.get_host_media()
+        # watermarked_file_path = self.embedding.embed_watermark(file_path)
+        # self.communication.send_file(watermarked_file_path)
 
     def receive_data_from_server(self):
-        received_file_path = self.communication.receive_file()
+        #received_file_path = self.communication.receive_file()
+        received_file_path = self.communication.client_socket.recv(1024)
         if received_file_path:
             print(f"Received file from server: {received_file_path}")
 
--- com/iot-digital-watermark/client/file_handler.py
+++ com/iot-digital-watermark/client/file_handler.py
@@ -1,20 +1,3 @@
-# server/file_handler.py
-
-class FileHandler:
-    def receive_file(self, client_socket):
-        file_name = 'data/received_file'
-        with open(file_name, 'wb') as file:
-            while True:
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                file.write(data)
-        return file_name
-
-    def process_and_store(self, file_name):
-        # Logic to move processed data to another directory
-        print(f"Processing and storing {file_name}...")
-
 # client/file_handler.py
 
 class FileHandler:
