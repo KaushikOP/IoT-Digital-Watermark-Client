b773964509c557a2add1575b0d63622c867d004d
KaushikOP <kaushiktalathi@gmail.com>
2024-12-12 23:32:57 +0530

    Update constants.py

--- com/iot-digital-watermark/utils/constants.py
+++ com/iot-digital-watermark/utils/constants.py
@@ -1,6 +1,6 @@
 # shared/constants.py
 
-DATA_DIR = 'data/'
+DATA_DIR = '/home/pi/IoT-Digital-Watermark-Client/data/'
 HOST_MEDIA_DIR = DATA_DIR + 'host_media/'
 WATERMARK_DIR = DATA_DIR + 'watermark/'
 WATERMARKED_MEDIA_DIR = DATA_DIR + 'watermarked_media/'
